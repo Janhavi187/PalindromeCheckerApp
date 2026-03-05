24bd0057690d26cb9f021a15f6c882e8d343279c
Janhavi Kishor <janhavikishor187@gmmail.com>
2026-03-05 09:16:23 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,14 +3,18 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("Palindrome Checker App");
-        System.out.println("Version: 1.0");
 
         String word = "madam";
 
-        if (word.equals("madam")) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is NOT a Palindrome");
+        if(word.equals(new StringBuilder(word).reverse().toString()))
+        {
+            System.out.println(word + " is a palindrome");
         }
+        else
+        {
+            System.out.println(word + " is not a palindrome");
+        }
+
     }
+
 }
